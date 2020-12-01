ef9adcb17cd906e5df5cfc14a349092d5266cd6f
Twebb97 <32673083+Twebb97@users.noreply.github.com>
2020-12-01 21:13:14 +0000

    Added day 1 solutions for AoC2020

--- AdventOfCode/Solutions/Year2020/Day01/Solution.cs
+++ AdventOfCode/Solutions/Year2020/Day01/Solution.cs
@@ -8,6 +8,19 @@ namespace AdventOfCode.Solutions.Year2020
     class Day01 : ASolution
     {
 
+        private static Hashtable readfile(String path) {
+            Hashtable my_hashtable = new Hashtable();
+            using (StreamReader sr = File.OpenText(path))
+            {
+                string s;
+                while ((s = sr.ReadLine()) != null)
+                {
+                    my_hashtable.Add(Int32.Parse(s), 2020 - Int32.Parse(s));
+                }
+                return my_hashtable;
+            }
+        }
+        
         public Day01() : base(01, 2020, "")
         {
 
@@ -15,12 +28,32 @@ namespace AdventOfCode.Solutions.Year2020
 
         protected override string SolvePartOne()
         {
-            return null;
+            Hashtable my_hashtable = readfile("./input.txt");
+            IDictionaryEnumerator denum = my_hashtable.GetEnumerator();
+            while (denum.MoveNext())
+            {
+                if (my_hashtable.ContainsKey(denum.Value)) {
+                    System.Console.WriteLine(Convert.ToInt32(denum.Key) * Convert.ToInt32(denum.Value));
+                    break;
+                }
+            }
         }
 
         protected override string SolvePartTwo()
         {
-            return null;
+            Hashtable my_hashtable = readfile("./input.txt");
+            IDictionaryEnumerator denum = my_hashtable.GetEnumerator();
+            IDictionaryEnumerator denum2 = my_hashtable.GetEnumerator();
+            while (denum.MoveNext())
+            {
+                while (denum2.MoveNext()) {
+                    if(my_hashtable.ContainsKey((Convert.ToInt32(denum.Value) - Convert.ToInt32(denum2.Key)))) {
+                        System.Console.WriteLine(Convert.ToInt32(denum.Key) * Convert.ToInt32(denum2.Key) * (Convert.ToInt32(denum.Value) - Convert.ToInt32(denum2.Key)));
+                        break;
+                    }
+                }
+                break;
+            }
         }
     }
 }
